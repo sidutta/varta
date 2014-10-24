e3cbb267e35f717635a51c70f594dd82be0c9f0a
sidutta <siddhartha_dutta@live.com>
2014-10-25 00:26:25 +0530

    event handler for smiley set

--- src/varta/view/LoginController.java
+++ src/varta/view/LoginController.java
@@ -16,45 +16,45 @@ public class LoginController {
 
 	@FXML
 	private Button loginButton;
-	
+
 	@FXML
 	private TextField userid;
-	
+
 	public static Client client;
-	
 
-     public LoginController() {
-    }
 
-    
-    @FXML
-    private void initialize() {
-    	
-    	loginButton.setOnAction((event) -> {
-    		String username=userid.getText();
+	public LoginController() {
+	}
+
+	@FXML
+	private void initialize() {
+
+		loginButton.setOnAction((event) -> {
+
+			String username=userid.getText();
 			System.out.println(username);
 			client=new Client("localhost",username,5002);
-			
-			
-	        try {
-	        	
-	        	Stage loginStage = (Stage) loginButton.getScene().getWindow();	        	
-	        	Stage chatStage=new Stage();
-	        	chatStage.setTitle( "Varta. Welcome " + client.getUsername() + "." );
-	            FXMLLoader loader = new FXMLLoader();
-	            loader.setLocation( getClass().getResource("ChatWindow.fxml") );
-	            AnchorPane chatpage = (AnchorPane) loader.load();
-	            Scene scene = new Scene( chatpage );
-	            chatStage.setScene( scene );
-	            loginStage.close();
-	            chatStage.show();
-	            
-	            
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
+
+			try {
+
+				Stage loginStage = (Stage) loginButton.getScene().getWindow();	        	
+				Stage chatStage=new Stage();
+				chatStage.setTitle( "Varta. Welcome " + client.getUsername() + "." );
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation( getClass().getResource( "ChatWindow.fxml" ) );
+				AnchorPane chatpage = (AnchorPane) loader.load();
+				Scene scene = new Scene( chatpage );
+				chatStage.setScene( scene );
+				loginStage.close();
+				chatStage.show();
+
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		});
-    }
-    
-    
-    }
+
+	}
+
+}
